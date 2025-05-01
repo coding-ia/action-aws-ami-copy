f19d01349fb5b7ac2baf67b9e4571787b9524d97
bjunker <brian.junker@gmail.com>
2025-05-01 07:25:54 -0700

    Updates

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,9 +3,11 @@
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json';
 
 const config = {
   input: 'src/index.ts',
+  plugins: [json()],
   output: {
     esModule: true,
     file: 'dist/index.js',
